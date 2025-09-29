42125fab3787abc4ae44af9458d5c0e39133e5ff
stanmqz <moscarstanley37@gmail.com>
2025-09-29 10:38:35 -0600

    Implement user input for calculating average grades and add temperature comparison feature

--- 24_9_25/ejercicio2.js
+++ 24_9_25/ejercicio2.js
@@ -1,29 +1,48 @@
 //solicitar al usuario tres nombres para guardarlos en el array y mostrar cada uno
 
-import readline from 'readline'
+import readline from "readline";
 
-const rl= readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-let nombres=[];
+// let nombres=[];
 
-rl.question("Ingrese el primer nombre:",(nombre1)=>{
-    nombres.push(nombre1);
+// rl.question("Ingrese el primer nombre:",(nombre1)=>{
+//     nombres.push(nombre1);
 
-    rl.question("Ingrese el segundo nombre:",(nombre2)=>{
-        nombres.push(nombre2);
+//     rl.question("Ingrese el segundo nombre:",(nombre2)=>{
+//         nombres.push(nombre2);
 
-        rl.question("Ingrese el tercer nombre:",(nombre3)=>{
-            nombres.push(nombre3);
+//         rl.question("Ingrese el tercer nombre:",(nombre3)=>{
+//             nombres.push(nombre3);
 
-            console.log("Los nombres ingresados son:");
-            console.log("Primer nombre:", nombres[0])
-            console.log("Segundo nombre:", nombres[1])
-            console.log("Tercer nombre:", nombres[2])
+//             console.log("Los nombres ingresados son:");
+//             console.log("Primer nombre:", nombres[0])
+//             console.log("Segundo nombre:", nombres[1])
+//             console.log("Tercer nombre:", nombres[2])
 
-            rl.close
-        })
-    })
-})
+//             rl.close
+//         })
+//     })
+// })
+
+let numeros = [];
+
+rl.question("Ingrese la primera nota: ", (nota1) => {
+  numeros[0] = parseInt(nota1);
+
+  rl.question("Ingrese la segunda nota: ", (nota2) => {
+    numeros[1] = parseInt(nota2);
+
+    rl.question("Ingrese la segunda nota: ", (nota3) => {
+      numeros[2] = parseInt(nota3);
+
+      let promedio = (numeros[0] + numeros[1], numeros[2]) / 3;
+      console.log(`El promedio es :${promedio}`);
+
+      rl.close()
+    });
+  });
+});
