05c7cdf7db2186026a4e1c2bad3c366e3ffb6190
Stan <moscarstanley37@gmail.com>
2025-09-29 15:20:06 -0600

    Dias

--- 24_9_25/array.js
+++ 24_9_25/array.js
@@ -6,3 +6,11 @@ const deTodo = ['Hola,', 10, null, true, {nombre: 'Stan', edad: 19}]
 console.log(deTodo)
 
 numeros[1] = 88
+
+numeros.splice(2,0,100)
+console.log(numeros)
+
+const dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+let primer = dias[0]
+let ultimo = dias[dias.length -1]
+console.log(ultimo)
