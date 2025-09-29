03e3ac0960cfd2d93be23cd7b03213a429a6ff2f
stanmqz <moscarstanley37@gmail.com>
2025-09-29 09:51:49 -0600

    Ejercicios arrays

---
+++ 24_9_25/ejercicio.js
@@ -0,0 +1,17 @@
+const carrito =[];
+
+const producto1 ={
+    nombre: "Camiseta",
+    precio: 2.50
+}
+
+carrito.push(producto1)
+console.log(carrito);
+
+const producto2 ={
+    nombre: "zapatos",
+    precio: 12.50
+}
+
+carrito.push(producto2)
+console.log(carrito)
